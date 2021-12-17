c1968b3333bb711b8616e678e056c8017170aa27
lukeheo8592 <71468858+lukeheo8592@users.noreply.github.com>
2021-12-17 13:39:49 -0500

    .

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,4 @@
-import { HashRouter,BrowserRouter, Switch, Route } from "react-router-dom";
+import {BrowserRouter, Switch, Route } from "react-router-dom";
 import Coin from "./routes/Coin";
 import Coins from "./routes/Coins";
 
